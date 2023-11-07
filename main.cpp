78c6e39addfc53a162e6c7ec2e6724f3b856262d
Advik <advik.b@gmail.com>
2023-11-07 19:47:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,44 @@
-#include "SDL.h"
+#include <SDL.h>
+#include <string>
 
-int main(int argc, char *argv[])
-{
-  SDL_Init(SDL_INIT_VIDEO);
+const int screen_x = 640;
+const int screen_y = 480;
+const std::string window_title = "SDL2 Window";
 
-  SDL_Window *window = SDL_CreateWindow(
-    "SDL2Test",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    window = SDL_CreateWindow(window_title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_x, screen_y, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  bool running = true;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // Game loop
+    bool running = true;
+    float color = 0.;
+    int dcolor; // Dynamically updating int version of color
+
+
     SDL_Event event;
     while (running) {
-        // Set background color to gray
+        // Slowly change the background color from black to white
+        dcolor = (int)color; // Cast float to int
+        SDL_SetRenderDrawColor(renderer, dcolor, dcolor, dcolor, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
-      while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-          running = false;
+        SDL_RenderPresent(renderer);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
         }
-      }
+        color += 0.01;
     }
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    return 0;
 }
